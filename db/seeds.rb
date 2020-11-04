41ddc996182186d210f464e4f428c5c06c07ea0b
newworldcountdown <larc.11.20@gmail.com>
2020-11-04 15:31:39 +0900

    seed user.create導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(admin: 1, name: "kota", email: "newworld@gmail.com", password: "u8JNGy9C", created_at: Time.zone.now, updated_at: Time.zone.now)
